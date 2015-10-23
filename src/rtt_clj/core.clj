0e01a743c34e81060dcb682088158668b87731fa
Gregory Haskins <gregory.haskins@gmail.com>
2015-10-23 15:11:59 -0400

    Baseline test
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/rtt_clj/core.clj
+++ src/rtt_clj/core.clj
@@ -1,7 +1,21 @@
 (ns rtt-clj.core
   (:gen-class))
 
+(defn timeMe [f & args]
+  (let [start  (System/nanoTime)
+        result (apply f args)
+        stop   (System/nanoTime)]
+    (vector result (- stop start))
+    )
+  )
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [nr 10000
+        func #(pr-str [1 :a 2 :b 3 :c])
+        [_, result] (timeMe #(loop [i 1]
+                               func
+                               (if (> i nr)
+                                 0
+                                 (recur (inc i)))))]
+    (println (double (/ result nr)))))
