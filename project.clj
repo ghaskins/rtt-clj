647f059c48a3b5132c49705f2970cefa73d0d04f
Gregory Haskins <gregory.haskins@gmail.com>
2015-10-24 11:10:32 -0400

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main rtt-clj.core
   :target-path "target/%s"
   :aot :all
--- src/rtt_clj/core.clj
+++ src/rtt_clj/core.clj
@@ -1,21 +1,39 @@
 (ns rtt-clj.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(defn timeMe [f & args]
+(def cli-options
+  ;; An option with a required argument
+  [["-i" "--iterations COUNT" "Iteration count"
+    :default 10000
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(> % 0)]]
+   ["-h" "--help"]])
+
+(defn timeOnce [f & args]
   (let [start  (System/nanoTime)
         result (apply f args)
         stop   (System/nanoTime)]
-    (vector result (- stop start))
-    )
-  )
+    (vector result (- stop start))))
 
-(defn -main
-  [& args]
-  (let [nr 10000
-        func #(pr-str [1 :a 2 :b 3 :c])
-        [_, result] (timeMe #(loop [i 1]
-                               func
-                               (if (> i nr)
-                                 0
-                                 (recur (inc i)))))]
-    (println (double (/ result nr)))))
+(defn timeMany [nr f & args]
+  (let [[_, result] (timeOnce #(loop [i 1]
+                                 (apply f args)
+                                 (if (> i nr)
+                                   nil
+                                   (recur (inc i)))))]
+    (double (/ result nr))))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn -main [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (cond
+      (:help options) (exit 0 summary))
+    (let [nr (:iterations options)
+          tests [["pr-str" #(pr-str [1 :a 2 :b 3 :c])]]]
+      (println "Running" nr "iterations across" (count tests) "test(s)")
+      (map (fn [[name func]] (println name ":" (timeMany nr func))) tests))))
